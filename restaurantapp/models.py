b3645d2e42e56da1eed7a7727fffe97a6ec3d650
Micheal Das <gokumicheal81@gmail.com>
2022-09-14 06:05:03 +0530

    Models created

--- restaurantapp/models.py
+++ restaurantapp/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+
+unit_choices = [
+    ("kg", "KG"),
+    ("gm", "Gram"),
+    ("lr", "Litre"),
+    ("ou", "Ounces"),
+]
+
+class Ingrediants(models.Model):
+    name = models.CharField(max_length=200)
+    available_quantity = models.IntegerField()
+    price_per_unit = models.FloatField()
+    unit = models.CharField(max_length=2, choices=unit_choices)
+
+
+class MenuItems(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.FloatField()
+
+
+class RecipeRequirements(models.Model):
+    menu = models.ForeignKey(MenuItems, on_delete=models.CASCADE)
+    ingrediant = models.ForeignKey(Ingrediants, on_delete=models.CASCADE)
+
+
+class PurchaseLog(models.Model):
+    menu = models.ForeignKey(MenuItems, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now=True)
+    quantity = models.FloatField(default=1)
