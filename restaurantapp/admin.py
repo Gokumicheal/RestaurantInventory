02dca94feafbe9063338696db0d7583d7d864959
Micheal Das <gokumicheal81@gmail.com>
2022-09-14 06:05:03 +0530

    Models created

--- restaurantapp/admin.py
+++ restaurantapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Ingrediants, MenuItems, RecipeRequirements, PurchaseLog
 
 # Register your models here.
+
+admin.site.register(Ingrediants)
+admin.site.register(MenuItems)
+admin.site.register(RecipeRequirements)
+admin.site.register(PurchaseLog)
--- restaurantapp/models.py
+++ restaurantapp/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+unit_choices = [
+    ("kg", "KG"),
+    ("gm", "Gram"),
+    ("lr", "Litre")
+]
+
+class Ingrediants(models.Model):
+    name = models.CharField(max_length=200)
+    available_quantity = models.IntegerField()
+    price_per_unit = models.IntegerField()
+    unit = models.CharField(max_length=2, choices=unit_choices)
+
+
+class MenuItems(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.IntegerField()
+
+
+class RecipeRequirements(models.Model):
+    menu = models.ForeignKey(MenuItems, on_delete=models.CASCADE)
+    ingrediant = models.ForeignKey(Ingrediants, on_delete=models.CASCADE)
+
+
+class PurchaseLog(models.Model):
+    menu = models.ForeignKey(MenuItems, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now=True)
+    quantity = models.IntegerField(default=1)
